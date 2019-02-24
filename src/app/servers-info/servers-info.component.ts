bbf300bd3e7987c80c3d7c6638f6c2f23e16db7b
fedeviotti <fedeviotti@gmail.com>
2019-02-24 11:39:11 +0100

    piccole modifiche servers-info component

--- src/app/servers-info/servers-info.component.ts
+++ src/app/servers-info/servers-info.component.ts
@@ -7,16 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersInfoComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = "No server created yet!";
+  serverCreationStatus = 'No server created yet!';
   serverName = 'TestServer';
 
-  constructor() { 
+  constructor() {
     // console.log('this out', this);
     // var that = this;
     // setTimeout( function () {
     //   console.log('this in', this);
     // }, 2000);
-    
     setTimeout( () => {
       this.allowNewServer = true;
     }, 2000);
@@ -26,7 +25,7 @@ export class ServersInfoComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "A server was created! Name is " + this.serverName;
+    this.serverCreationStatus = 'A server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName (event: Event) {
